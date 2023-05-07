17ca15c112be54e501724fb9ca4e9514b38b0ca5
NikitaStarmoussov <117869986+NikitaStarmoussov@users.noreply.github.com>
2023-05-07 21:28:14 +0500

    fix: fix bug with exit in 2 tasc

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,15 +6,15 @@ const output = fs.createWriteStream(dir);
 stdout.write(`text yours text)\n`);
 stdin.on('data', data => {
   if(data.toString().trim() === 'exit'){
-    stdout.write(`goodbye\n`);
     exit();
   }else{
     output.write(data);
   }
 })
 process.on('SIGINT', ()=>{
+  exit();
+})
+process.on('exit', ()=>{
   stdout.write(`\ngoodbye\n`);
   exit();
 })
-// input.on('data', chunk => output.write(chunk));
-// input.on('error', error => console.log('Error', error.message));
