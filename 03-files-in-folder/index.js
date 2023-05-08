5429f644c7daaca2da3c8c5af82bad6e5f6bbf18
NikitaStarmoussov <117869986+NikitaStarmoussov@users.noreply.github.com>
2023-05-08 06:32:02 +0500

    feat: full work task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,15 +1,42 @@
-const fs = require('fs');
-const path = require('path');
-
-
-const dirSecretFolder = path.join(__dirname, 'secret-folder'); 
-console.log(dirSecretFolder);
-fs.readdir(dirSecretFolder, { withFileTypes: true },function (error, array){
-  if (error) {
-    throw error;
-  } else {
-    console.log(array)
-  }
-})
-
+const fs = require("fs");
+const path = require("path");
+//require modulees
+const dirSecretFolder = path.join(__dirname, "secret-folder");
+//add correct path
+function getInfo(pathFolder) {
+  fs.readdir(pathFolder, { withFileTypes: true }, (error, array) => {
+    if (error) {
+      throw error;
+    } else {
+      const filesArr = array
+        .filter((file) => file.isFile())
+        .map((file) => file.name);
+      for (let i = 0; i < filesArr.length; i++) {
+        fs.stat(path.join(pathFolder, filesArr[i]), (error, stats) => {
+          if (error) {
+            throw error;
+          } else {
+            let name = path.basename(filesArr[i], path.extname(filesArr[i]));
+            let ext = "";
+            let size = "0B";
+            if (name[0] !== ".") {
+              ext = path.extname(filesArr[i]).slice(1);
+            } else {
+              name = "Not have name";
+              ext = path
+                .basename(filesArr[i], path.extname(filesArr[i]))
+                .slice(1);
+            }
+            if (stats.size) {
+              size = stats.size.toString() + "B";
+            }
+            let result = name + " - " + ext + " - " + size;
+            console.log(result);
+          }
+        });
+      }
+    }
+  });
+}
+getInfo(dirSecretFolder);
 
