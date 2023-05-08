346044eb5b015bee019aca0e32a1753b3092f9dc
NikitaStarmoussov <117869986+NikitaStarmoussov@users.noreply.github.com>
2023-05-08 07:27:29 +0500

    feat: add full task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+"use strict";
 
+const fs = require("fs/promises");
+const path = require("path");
+
+function copyFolder(dir, dirCopy) {
+  // delete files-copy folder
+  fs.rm(dirCopy, {
+    recursive: true,
+    force: true,
+  }).finally(function () {
+    //if we nothing for delete we always start with finally
+    fs.mkdir(dirCopy, { recursive: true });
+    //create folder
+    fs.readdir(dir, { withFileTypes: true }),
+      (error, array) => {
+        if (error) {
+          throw error;
+        } else {
+          array.forEach(function (item) {
+            if (item.isFile()) {
+              fs.copyFile(
+                path.join(dir, item.name),
+                path.join(dirCopy, item.name)
+              );
+            }
+          });
+        }
+      };
+  });
+}
+
+const folder = path.join(__dirname, "files");
+const folderCopy = path.join(__dirname, "files-copy");
+
+copyFolder(folder, folderCopy);
