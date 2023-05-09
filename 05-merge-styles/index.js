431941b23cf2c1b5b59f344d06d820fa2a84d406
NikitaStarmoussov <117869986+NikitaStarmoussov@users.noreply.github.com>
2023-05-09 06:12:28 +0500

    feat: full task 4

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+
+function buildCss(pathFolder) {
+  fs.writeFile(
+    path.join(__dirname, "project-dist", "bundle.css"),
+    "",
+    (error) => {
+      if (error) {
+        throw error;
+      }
+    }
+  );
+
+  fs.readdir(pathFolder, { withFileTypes: true }, (error, array) => {
+    if (error) {
+      throw error;
+    } else {
+      const filesArray = array
+        .filter((file) => file.isFile())
+        .map((file) => file.name);
+      for (let i = 0; i < filesArray.length; i++) {
+        fs.stat(path.join(pathFolder, filesArray[i]), (error, stats) => {
+          let ext = path.extname(filesArray[i]).slice(1);
+          if (ext === "css") {
+            let filePath = path.join(pathFolder, filesArray[i]);
+            fs.readFile(filePath, "utf-8", (error, fileContent) => {
+              if (error) {
+                throw error;
+              } else {
+                fs.appendFile(
+                  path.join(__dirname, "project-dist", "bundle.css"),
+                  `${fileContent}${String.fromCharCode(0x0a, 0x0a)}`,
+                  (error) => {
+                    if (error) {
+                      throw error;
+                    }
+                  }
+                );
+              }
+            });
+          }
+        });
+      }
+    }
+  });
+}
+
+const folder = path.join(__dirname, "styles");
+
+buildCss(folder);
